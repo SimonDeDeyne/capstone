98074a17546d9bff7343bf0f5506b938f2671458
Simon De Deyne <simon2d@gmail.com>
2018-08-26 12:03:47 +1000

    Updated colour and lingclass

--- README.md
+++ README.md
@@ -16,10 +16,10 @@ If you make changes to the scripts and want to test, you will want to use the br
 In Chrome you can access the [Development Tools](https://developers.google.com/web/tools/chrome-devtools/) panel by pressing <code>Ctrl + i</code>. 
 
 ## Obtaining the data
-The experiments provided here are for testing only. They will not write data to the server, but you can manually inspect the data
-and copy into a text or Excel file. The actual experiments will include some additional code to save the responses to the server.
-For the colour experiment, you can obtain the data by entering <code>jsPsych.data.get().filter({test_part: 'visualSearch'}).csv()</code> into the Console Bar using Chrome (use <code>Ctrl + i</code> to activate it).
-(Note: there's an easier way to save data locally, I'll fix that soon)
+The experiments provided here are for testing only. They will not write data to a remote server,  but to a local <code>.csv</code> file.
+You can easily copy the content of that file to Excel. 
+The actual experiments will include some additional code to save the responses to the server.
+
 
 ## Hints for collecting accurate data
 Millisecond accuracy can be hard to obtain using online experiments in some cases. However, recent work shows that web-browser-based experiments can still be used to obtain accurate RTs ([de Leeuw, Motz, 2015](https://link.springer.com/article/10.3758/s13428-015-0567-2)). The biggest risk is having the task interrupted with all kinds of notifications or processes running in the background. That's why it's best to make sure there no other programs or tabs open and disabling programs running in the background (Dropbox, Facebook, etc.).
--- lingclass/js/lingclass.js
+++ lingclass/js/lingclass.js
@@ -1,94 +1,111 @@
 /*
-Stimuli can be obtained from https://crl.ucsd.edu/experiments/ipnp/
-https://www.bcbl.eu/databases/multipic
-
-% some ideas for the sketch project: http://cybertron.cg.tu-berlin.de/eitz/projects/classifysketch/
-
-
+Experiment on lexical processing effects of visually presented words in English or Mandarin.
 */
 
-var timeline = [];
-/*
-var categorization_trial = {
-    type: 'categorize-html',
-    stimulus: '<div><img src="img/obj045bird.gif"><img src="img/obj045bird.gif"></div><br><img src="img/obj045bird.gif">',
-    text_answer: 'letter',
-      choices: ['f', 'j'],
-    correct_text: "",
-    incorrect_text: "",
-    show_stim_with_feedback: false,
-    prompt: "<p>Which item is most related left (press f) or right (press j).</p>"
-};
-*/
+
 var instructions = {
     type: 'instructions',
     pages: [
         'Welcome to the experiment.<br> Here are the instructions',
         'Here\'s an example ',
-        'Click next to begin.',
+        'Click next to begin.'
     ],
     show_clickable_nav: true
+};
+
+
+
+var test_stimuli = [{
+        stimulus: { cue: './img/1.png', targetA: './img/2.png', targetB: './img/3.png' },
+        verbalStimulus: '2 3 4 5 6',
+        prompt: '',
+        data: { test_part: 'vistriad' }
+    },
+    {
+        stimulus: { cue: './img/2.png', targetA: './img/4.png', targetB: './img/3.png' },
+        verbalStimulus: '2 3 4 5 6',
+        prompt: "<br><p class='prompt'>Press <em>Space</em> if item is same.</p>",
+        data: { test_part: 'vistriad' }
+    },
+    {
+        stimulus: { cue: './img/3.png', targetA: './img/1.png', targetB: './img/5.png' },
+        verbalStimulus: '2 3 4 5 6',
+        prompt: "<br><p class='prompt'>Press <em>Space</em> if item is same.</p>",
+        data: { test_part: 'vistriad' }
+    }
+];
+
+
+// Counterbalance position of A and B
+function counterBalanceStimuli(stimuli) {
+    'use strict';
+    var m = new MersenneTwister();
+    var tmp = '';
+
+    for (s in stimuli) {
+        console.log(stimuli[s].stimulus.targetA);
+
+        if (m.random() > 0.5) {
+            tmp = stimuli[s].stimulus.targetA;
+            stimuli[s].stimulus.targetA = stimuli[s].stimulus.targetB;
+            stimuli[s].stimulus.targetB = tmp;
+        }
+    }
+    return stimuli;
 }
 
-/*
-var twoAFC_trial = {
-    type: 'vistriad',
-    stimulus: {cue: './img/1.png', targetA: './img/2.png', targetB: './img/3.png'}
-
-}*/
-
-var test_stimuli = [
-    {stimulus: {cue: './img/1.png', targetA: './img/2.png', targetB: './img/3.png'},verbalStimulus: '2 3 4 5 6',prompt: ''},
-    {stimulus: {cue: './img/2.png', targetA: './img/4.png', targetB: './img/3.png'},verbalStimulus: '2 3 4 5 6',prompt: "<br><p class='prompt'>Press <em>Space</em> if item is same.</p>"},
-    {stimulus: {cue: './img/3.png', targetA: './img/1.png', targetB: './img/5.png'},verbalStimulus: '2 3 4 5 6',prompt: "<br><p class='prompt'>Press <em>Space</em> if item is same.</p>"}
-];
 
 
-/*var digit_stimuli = [
-    {verbalStimulus: '2 3 4 5 6'},
-    {verbalStimulus: '2 3 5 4 6'},
-    {verbalStimulus: '2 3 4 5 6'},
-];
-*///, digitSpan: '2 3 4 5 6'
+function saveData() {
+    var timeStamp = Math.floor(Date.now());
+    //jsPsych.data.get().filter({ test_part: 'fixation' }).localSave('csv', 'lingclass' + timeStamp + '.csv');
+    jsPsych.data.get().localSave('csv', 'lingclass' + timeStamp + '.csv');
 
+}
 
 var test = {
     type: 'vistriad',
     stimulus: jsPsych.timelineVariable('stimulus'),
     data: jsPsych.timelineVariable('data')
-}
+};
 
 var verbal = {
     type: 'html-keyboard-response',
     stimulus: jsPsych.timelineVariable('verbalStimulus'),
     trial_duration: 2000,
     response_ends_trial: false,
-    choices: 32,
+    choices: jsPsych.ALL_KEYS,
+    on_finish: function(data) {
+        if (data.key_press) {
+            console.log(data.key_press);
+        }
+        
+    },
+    data: {test_part: 'verbal'},
     prompt: jsPsych.timelineVariable('prompt')
-}
+};
 
 
 var fixation = {
     type: 'html-keyboard-response',
-    stimulus: [      
-            '<div class="content">',
-            '<div class="row">',
-              '<div class="cell" id="targetA">',
-              '</div>',
-              '<div class="cell" id="targetB">',
-              '</div>',
-            '</div>',
-            '<div class="row">',
-            '<div class="cell" id="cue"><p>+</p></div>',
-            '</div>',
-          '</div>',
-        ].join(''),
+    stimulus: [
+        '<div class="content">',
+        '<div class="row">',
+        '<div class="cell" id="targetA">',
+        '</div>',
+        '<div class="cell" id="targetB">',
+        '</div>',
+        '</div>',
+        '<div class="row">',
+        '<div class="cell" id="cue"><p>+</p></div>',
+        '</div>',
+        '</div>'
+    ].join(''),
     choices: jsPsych.NO_KEYS,
     trial_duration: 1000,
     trial_duration: function() {
         return jsPsych.randomization.sampleWithoutReplacement([750, 1000, 1250], 1)[0];
     },
-    //prompt: "<br><p class='prompt'></p>",
     prompt: '',
     data: { test_part: 'fixation' }
 };
@@ -96,7 +113,8 @@ var fixation = {
 var debrief_block = {
     type: "html-keyboard-response",
     stimulus: function() {
-        return "<p>Good job! You deserve a one minute break.</p>";
+        return "<p>Good job! You deserve a one minute break.</p><br>" +
+            "<p>Press any key to proceed</p>";
     }
 };
 
@@ -109,34 +127,35 @@ var debrief_experiment = {
 };
 
 
-
+// Experiment blocks: note randomization is done in the stimulus generation phase
 var block_1 = {
     timeline: [verbal, fixation, test],
     //timeline_variables: test_stimuli.slice(0, 4, 1),
     timeline_variables: test_stimuli,
-    randomize_order: true,
+    randomize_order: false,
     repetitions: 1
-}
+};
 
 
 var block_2 = {
     timeline: [fixation, test],
     //timeline_variables: test_stimuli.slice(0, 4, 1),
     timeline_variables: test_stimuli,
-    randomize_order: true,
+    randomize_order: false,
     repetitions: 1
-}
+};
 
-
-
-timeline.push(block_1,debrief_block,block_2,debrief_experiment);
-
+var timeline = [];
+timeline.push(instructions, block_1, debrief_block, block_2, debrief_experiment);
 
 
 jsPsych.init({
     timeline: timeline,
     on_finish: function() {
-        jsPsych.data.displayData();
+        //jsPsych.data.displayData();
+        saveData();
+        console.log('Experiment finished.');
+
     }
 });
 // show the images
